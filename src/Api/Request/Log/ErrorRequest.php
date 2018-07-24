2cb1d713fec2d56962b2f5e8642c5ef8614c5cc2
Florian Moser <products@famoser.ch>
2018-07-24 09:36:22 +0200

    adding log endpoint for js

---
+++ src/Api/Request/Log/ErrorRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the mangel.io project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Api\Request\Log;
+
+class ErrorRequest
+{
+    private $message;
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage($message): void
+    {
+        $this->message = $message;
+    }
+}
