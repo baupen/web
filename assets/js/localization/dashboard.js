a89eae02f7f5527caebf272760511a91caed6219
Florian Moser <products@famoser.ch>
2018-08-03 18:25:17 +0200

    adding dashboard translation
    Translation to it

--- assets/js/localization/dashboard.js
+++ assets/js/localization/dashboard.js
@@ -28,5 +28,35 @@ export default {
                 add_new: "Neu erfassen"
             }
         }
+    },
+    it: {
+        register: {
+            name: "Pendenzen",
+            status_actions: {
+                open: "Offen",
+                overdue: "Frist überschritten",
+                to_inspect: "Zur Inspektion",
+                marked: "Markiert"
+            }
+        },
+        dialog: {
+            new_issues_in_foyer: "Eine neue Pendenz | {count} neue Pendenzen",
+            add_to_register: "Jetzt zum Verzeichnis hinzufügen"
+        },
+        feed: {
+            name: "Feed",
+            show_more: "Mehr anzeigen",
+            entries: {
+                response_received: "{craftsman} hat eine Pendenz beantwortet. | {craftsman} hat {count} Pendenzen beantwortet.",
+                visited_webpage: "{craftsman} hat die Pendenzen angeschaut.",
+                overdue_limit: "{craftsman} hat die Frist vom %limit% bei {count} Pendenzen überschritten.",
+            }
+        },
+        notes: {
+            name: "Notizen",
+            actions: {
+                add_new: "Neu erfassen"
+            }
+        }
     }
 }
