8e700994c798863b8e7e3b0189ae632e21d23e80
Florian Moser <products@famoser.ch>
2018-12-24 17:35:59 +0100

    resolves #100

---
+++ src/Service/CronService.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the mangel.io project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Service;
+
+use App\Service\Interfaces\CronServiceInterface;
+use App\Service\Interfaces\FileSystemSyncServiceInterface;
+use App\Service\Interfaces\PathServiceInterface;
+use Psr\Log\LoggerInterface;
+
+class CronService implements CronServiceInterface
+{
+    /**
+     * @var FileSystemSyncServiceInterface
+     */
+    private $fileSystemSyncService;
+
+    /**
+     * @var PathServiceInterface
+     */
+    private $pathService;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(FileSystemSyncServiceInterface $fileSystemSyncService, PathServiceInterface $pathService, LoggerInterface $logger)
+    {
+        $this->fileSystemSyncService = $fileSystemSyncService;
+        $this->pathService = $pathService;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @return bool
+     */
+    public function run()
+    {
+        $this->callScripts();
+        $this->fileSystemSyncService->sync();
+
+        return true;
+    }
+
+    /**
+     * looks for all scripts at an agreed by location / name and calls them.
+     */
+    private function callScripts()
+    {
+        $scriptsRoot = $this->pathService->getScriptsRoot();
+        $constructionSiteRoot = $this->pathService->getConstructionSiteFolderRoot();
+        $dirs = glob($scriptsRoot . \DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR);
+        foreach ($dirs as $dir) {
+            $possibleScriptName = $dir . \DIRECTORY_SEPARATOR . 'runner.py';
+            if (file_exists($possibleScriptName)) {
+                $command = $possibleScriptName . ' -d ' . $constructionSiteRoot;
+                exec($possibleScriptName . ' -d ' . $constructionSiteRoot, $output, $exitCode);
+                $this->logger->info('command ' . $command . ' terminated with exit code ' . $exitCode, ['full_output' => $output]);
+            }
+        }
+    }
+}
