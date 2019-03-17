d397a1185435fbd1b5a5566e8d57d7fa766cae5d
Florian Moser <products@famoser.ch>
2019-03-17 09:43:38 +0100

    resolves #147

--- assets/js/localization/switch.js
+++ assets/js/localization/switch.js
@@ -9,6 +9,12 @@ export default {
     },
     actions: {
       switch: 'Zur Baustelle'
+    },
+    messages: {
+      activate_construction_site: 'Aktivieren Sie eine Baustelle aus der Liste unten, um auf diese zuzugreifen.'
+    },
+    construction_site: {
+      activated: 'aktiviert'
     }
   },
   it: {
