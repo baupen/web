df51c5650cd28d406158052c066636299cc9f5f0
Florian Moser <products@famoser.ch>
2019-04-26 11:20:56 +0200

    resolve #183

--- assets/js/localization/share/filter.js
+++ assets/js/localization/share/filter.js
@@ -1,6 +1,7 @@
 export default {
   de: {
-    issues_overview: 'Pendenzenüberblick'
+    issues_overview: 'Pendenzenüberblick',
+    no_issues: 'Keine Pendenzen verfügbar'
   },
   it: {
     issues_overview: 'Panoramica delle pendenze'
