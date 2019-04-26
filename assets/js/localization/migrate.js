91b9c9cd9cfe1c0cdc8f32a5c673ba4968550854
Florian Moser <products@famoser.ch>
2019-04-26 14:17:33 +0200

    Revert "one step back"
    This reverts commit 65937bce50d5f3fa67692d20860349732a1ba538.

--- assets/js/localization/migrate.js
+++ assets/js/localization/migrate.js
@@ -1,11 +1,45 @@
 const fs = require('fs');
 const dashboardDe = require('./dashboard.de');
 
+fs.readdir(".", {}, function (err, filenames) {
+  if (err) {
+    throw new Error("failed to read directory");
+  }
+  console.log("found " + filenames.length + " files");
 
-parseJsObjectToJson(dashboardDe);
+  filenames.filter(f => f !== "migrate.js" && f.endsWith(".js")).forEach(filename => {
+    parseModuleExportToJsObject(filename);
+  })
+});
 
-function parseJsObjectToJson(object, name) {
-  fs.writeFile(name + ".json", JSON.stringify(obj, null, 2), (err) => {
+function parseModuleExportToJsObject(filename) {
+  fs.readFile(filename, "utf8", function (err, content) {
+    if (err) {
+      throw new Error("failed to load file " + filename);
+    }
+    console.log("read " + filename);
+
+    let newContent = content.substring(content.indexOf("{"));
+
+    const newFilename = filename.replace(".js", ".raw.js");
+    fs.writeFile(newFilename, newContent, 'utf8', function (err) {
+      if (err) {
+        throw new Error("writing to file failed");
+      }
+
+      console.log("converted " + filename + " to js object");
+      parseJsObjectToJson(newFilename);
+    })
+  });
+}
+
+
+function parseJsObjectToJson(filename) {
+  const obj = require("./" + filename);
+  console.log("read " + filename + " as js object");
+
+  const newFilename = filename.replace(".raw.js", ".json");
+  fs.writeFile(newFilename, JSON.stringify(obj, null, 2), (err) => {
     if (err) {
       throw new Error("writing json to file failed");
     }
