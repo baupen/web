65937bce50d5f3fa67692d20860349732a1ba538
Florian Moser <products@famoser.ch>
2019-04-26 14:10:11 +0200

    one step back

--- assets/js/localization/migrate.js
+++ assets/js/localization/migrate.js
@@ -1,44 +1,11 @@
 const fs = require('fs');
 
-fs.readdir(".", {}, function (err, filenames) {
-  if (err) {
-    throw new Error("failed to read directory");
-  }
-  console.log("found " + filenames.length + " files");
+import dashboardDe from './dashboard.de';
 
-  filenames.filter(f => f !== "migrate.js" && f.endsWith(".js")).forEach(filename => {
-    parseModuleExportToJsObject(filename);
-  })
-});
+parseJsObjectToJson(dashboardDe);
 
-function parseModuleExportToJsObject(filename) {
-  fs.readFile(filename, "utf8", function (err, content) {
-    if (err) {
-      throw new Error("failed to load file " + filename);
-    }
-    console.log("read " + filename);
-
-    let newContent = content.substring(content.indexOf("{"));
-
-    const newFilename = filename.replace(".js", ".raw.js");
-    fs.writeFile(newFilename, newContent, 'utf8', function (err) {
-      if (err) {
-        throw new Error("writing to file failed");
-      }
-
-      console.log("converted " + filename + " to js object");
-      parseJsObjectToJson(newFilename);
-    })
-  });
-}
-
-
-function parseJsObjectToJson(filename) {
-  const obj = require("./" + filename);
-  console.log("read " + filename + " as js object");
-
-  const newFilename = filename.replace(".raw.js", ".json");
-  fs.writeFile(newFilename, JSON.stringify(obj, null, 2), (err) => {
+function parseJsObjectToJson(object, name) {
+  fs.writeFile(name + ".json", JSON.stringify(obj, null, 2), (err) => {
     if (err) {
       throw new Error("writing json to file failed");
     }
