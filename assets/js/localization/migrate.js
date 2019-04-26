99b71419108985fbd45098a2a5c2476bc0806013
Florian Moser <products@famoser.ch>
2019-04-26 13:54:59 +0200

    write migration script

---
+++ assets/js/localization/migrate.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+
+fs.readdir(".", {}, function (err, files) {
+  if (err) {
+    throw new Error("failed to read directory");
+  }
+  console.log("found " + files.length + " files");
+
+  files.filter(f => f !== "migrate.js").forEach(file => {
+    parseModuleExportToJsObject(file);
+  })
+});
+
+function parseModuleExportToJsObject(file) {
+  fs.readFile(file, "utf8", function (err, content) {
+    if (err) {
+      throw new Error("failed to load file");
+    }
+    console.log("read " + file);
+
+    console.log(content);
+    let newContent = content.substring(content.indexOf("{"));
+
+    fs.writeFile(file, newContent, function (err) {
+      if (err) {
+        throw new Error("writing to file failed");
+      }
+
+      console.log("converted " + file + " to js object");
+      parseJsObjectToJson(file);
+    })
+  });
+}
+
+
+function parseJsObjectToJson(file) {
+  const obj = require("./" + file);
+  console.log("read " + file + " as js object");
+  fs.writeFile("dashboard.de.json", JSON.stringify(obj, null, 2), (err) => {
+    if (err) {
+      throw new Error("writing json to file failed");
+    }
+
+    console.log("converted " + file + " to json");
+  });
+}
