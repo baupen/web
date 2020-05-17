68b50e905dcd71f428d56258ef9b8cde45873211
Florian Moser <git@famoser.ch>
2020-05-17 10:18:32 +0200

    update frontend deps

--- assets/js/legacy.js
+++ assets/js/legacy.js
@@ -1,21 +1,5 @@
-import 'core-js/es6/promise';
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/weak-map';
-import 'core-js/es6/set';
-
 const $ = require('jquery');
-var compatibleBrowser = typeof Object['__defineSetter__'] === 'function';
+const compatibleBrowser = typeof Object['__defineSetter__'] === 'function';
 if (!compatibleBrowser) {
   $('#outdated').addClass('active');
 }
