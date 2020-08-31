931d77ba14e2e58a867fae76f79b81924baf4abb
Florian Moser <git@famoser.ch>
2020-08-31 09:22:59 +0200

    Clean up public folder

---
+++ assets/js/vuejs.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import BootstrapVue from 'bootstrap-vue';
+
+import { setInteractionMode } from 'vee-validate';
+
+setInteractionMode('lazy');
