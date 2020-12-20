b49ad0a7456d9110a178b671f0de25ceae74db33
Florian Moser <products@famoser.ch>
2020-12-20 11:09:54 +0100

    Correct report schema

---
+++ src/Api/Swagger/SwaggerDecorator.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the mangel.io project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Api\Swagger;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class SwaggerDecorator implements NormalizerInterface
+{
+    private $decorated;
+
+    public function __construct(NormalizerInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $docs = $this->decorated->normalize($object, $format, $context);
+
+        $pdfContent = [
+            'application/pdf' => [
+                'schema' => [
+                    'type' => 'string',
+                    'format' => 'binary',
+                ],
+            ],
+        ];
+
+        $docs['paths']['/api/issues/report']['get']['responses']['200']['content'] = $pdfContent;
+
+        return $docs;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+}
