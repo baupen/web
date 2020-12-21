5c95af1e89f35f9fb53acfbc87e42c90496d175d
Florian Moser <git@famoser.ch>
2020-12-21 17:42:05 +0100

    Improve date time

--- assets/js/vue/components/Shared/HumanReadableDate.vue
+++ assets/js/vue/components/Shared/HumanReadableDate.vue
@@ -20,8 +20,15 @@ export default {
       return moment(this.value);
     },
     fromNowDayGranularity: function () {
-      const now = moment();
-      return now < moment(this.value).add('hours', 22) ? 'today' : moment(this.value).fromNow()
+      const today = moment().startOf('day');
+      if (today <= this.momentDateTime) {
+        return this.$t("view.today");
+      }
+      const yesterday = today.subtract(1, 'days');
+      if (yesterday <= this.momentDateTime) {
+        return this.$t("view.yesterday");
+      }
+      return moment(this.value).fromNow()
     }
   },
   mounted() {
