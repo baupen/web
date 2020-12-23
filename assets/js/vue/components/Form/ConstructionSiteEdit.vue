51615b35ac4a227d1192396c0a59f2928f4ba848
Florian Moser <products@famoser.ch>
2020-12-23 09:49:23 +0100

    Detect similar construction names & warn user

--- assets/js/vue/components/Form/ConstructionSiteEdit.vue
+++ assets/js/vue/components/Form/ConstructionSiteEdit.vue
@@ -4,7 +4,16 @@
                v-model="modelValue.name"
                @input="emitUpdate"
                @valid="validProperties.name = $event"
-               :focus="true" />
+               :focus="true">
+      <div>
+          <p v-if="similarConstructionSiteNames.length > 0" class="text-primary">
+          <small>
+            {{$t('switch.similar_already_existing_construction_sites')}}: {{ similarConstructionSiteNames.join(",") }}
+          </small>
+        </p>
+      </div>
+    </text-edit>
+
 
     <text-edit id="streetAddress" :label="$t('construction_site.street_address')"
                v-model="modelValue.streetAddress"
@@ -27,6 +36,7 @@
 
 <script>
 import TextEdit from "./TextEdit";
+import debounce from "lodash.debounce"
 
 export default {
   components: {TextEdit},
@@ -38,7 +48,8 @@ export default {
         streetAddress: false,
         postalCode: false,
         locality: false,
-      }
+      },
+      similarConstructionSiteNames: []
     }
   },
   props: {
@@ -53,7 +64,10 @@ export default {
   watch: {
     isValid: function () {
       this.emitValid();
-    }
+    },
+    'modelValue.name': debounce(function (newVal) {
+      this.determineSimilarConstructionSiteNames(newVal)
+    }, 200)
   },
   methods: {
     emitUpdate: function () {
@@ -61,6 +75,42 @@ export default {
     },
     emitValid: function () {
       this.$emit('valid', this.isValid)
+    },
+    determineSimilarConstructionSiteNames: function (newName) {
+      if (!newName || newName.length < 4) {
+        return this.similarConstructionSiteNames = [];
+      }
+
+      const maxDistanceForWarning = Math.sqrt(newName.length)
+
+      let similarConstructionSites = this.constructionSites.map(constructionSite => ({
+        constructionSite,
+        distance: this.levenshteinDistance(newName, constructionSite.name)
+      })).filter(similarConstructionSite => similarConstructionSite.distance < maxDistanceForWarning)
+          .sort(((a, b) => a.distance - b.distance))
+
+      this.similarConstructionSiteNames = similarConstructionSites.map(similarConstructionSite => similarConstructionSite.constructionSite.name).slice(0, 3)
+    },
+    levenshteinDistance: function (a, b) {
+      // source: https://gist.github.com/andrei-m/982927#gistcomment-1797205
+      let m = [], i, j, min = Math.min;
+
+      if (!(a && b)) return (b || a).length;
+
+      for (i = 0; i <= b.length; m[i] = [i++]) ;
+      for (j = 0; j <= a.length; m[0][j] = j++) ;
+
+      for (i = 1; i <= b.length; i++) {
+        for (j = 1; j <= a.length; j++) {
+          m[i][j] = b.charAt(i - 1) === a.charAt(j - 1)
+              ? m[i - 1][j - 1]
+              : m[i][j] = min(
+                  m[i - 1][j - 1] + 1,
+                  min(m[i][j - 1] + 1, m[i - 1][j] + 1))
+        }
+      }
+
+      return m[b.length][a.length];
     }
   },
   computed: {
