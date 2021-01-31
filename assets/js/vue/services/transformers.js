843354b8ffeea7a4374d64187f259b7dea1e39e0
Florian Moser <git@famoser.ch>
2021-01-31 10:21:38 +0100

    Extract algorithm for reuse

---
+++ assets/js/vue/services/transformers.js
@@ -0,0 +1,54 @@
+const mapTransformer = {
+  _getChildren: function (key, parentLookup) {
+    if (!(key in parentLookup)) {
+      return []
+    }
+
+    return parentLookup[key].map(entry => ({
+      entity: entry,
+      children: this._getChildren(entry['@id'], parentLookup)
+    }))
+  },
+  _sortChildren: function (children) {
+    children.sort((a, b) => a.entity.name.localeCompare(b.entity.name))
+    children.forEach(child => {
+      this._sortChildren(child.children)
+    })
+  },
+  _flattenChildren: function (children, level = 0) {
+    let result = []
+    children.forEach(child => {
+      result.push({
+        entity: child.entity,
+        level
+      })
+      result = result.concat(...this._flattenChildren(child.children, level + 1))
+    })
+
+    return result
+  },
+  _hierarchy: function (maps) {
+    const rootKey = 'root'
+
+    const parentLookup = {}
+    maps.forEach(m => {
+      const parentKey = m.parent ?? rootKey
+
+      if (!parentLookup[parentKey]) {
+        parentLookup[parentKey] = []
+      }
+
+      parentLookup[parentKey].push(m)
+    })
+
+    return this._getChildren(rootKey, parentLookup)
+  },
+  flatHierarchy: function (maps) {
+    const hierarchy = this._hierarchy(maps)
+    this._sortChildren(hierarchy)
+
+    return this._flattenChildren(hierarchy)
+  }
+}
+
+export { mapTransformer }
