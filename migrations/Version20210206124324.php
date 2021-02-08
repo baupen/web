fb953fd41e8ce9ae890b55227b86876b7eceeeeb
Florian Moser <git@famoser.ch>
2021-02-08 10:20:41 +0100

    Workaround for doctrine/migrations#1104

--- migrations/Version20210206124324.php
+++ migrations/Version20210206124324.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the mangel.io project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -12,20 +21,25 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20210206124324 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE filter ADD last_used_at DATETIME DEFAULT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE filter DROP last_used_at');
+    }
+
+    public function isTransactional(): bool
+    {
+        return false;
     }
 }
