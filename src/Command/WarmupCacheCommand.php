47bd832c4f0ec4d39ed8f6a8458f8a0f01988d2f
Florian Moser <git@famoser.ch>
2021-02-14 16:23:50 +0100

    Fix typo

--- src/Command/WarmupCacheCommand.php
+++ src/Command/WarmupCacheCommand.php
@@ -21,7 +21,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class WarumupCacheCommand extends Command
+class WarmupCacheCommand extends Command
 {
     /**
      * @var ManagerRegistry
