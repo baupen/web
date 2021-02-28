d38860537dc1f84772142ac9d0d82a451259fdd7
Florian Moser <git@famoser.ch>
2021-02-28 14:36:10 +0100

    Style chart

--- assets/js/vue/components/DashboardIssuesGraph.vue
+++ assets/js/vue/components/DashboardIssuesGraph.vue
@@ -7,10 +7,12 @@
 <script>
 
 import Chart from 'chart.js'
+import ButtonWithModalConfirm from './Library/Behaviour/ButtonWithModalConfirm'
 
-Chart.platform.disableCSSInjection = true;
+Chart.platform.disableCSSInjection = true
 
 export default {
+  components: { ButtonWithModalConfirm },
   data () {
     return {
       chart: null
@@ -22,46 +24,107 @@ export default {
       required: true
     }
   },
-  computed: {
-  },
+  computed: {},
   mounted () {
-    const ctx = this.$refs.chart.getContext('2d');
+    const exampleData = [
+      {
+        date: '2020-01-31',
+        open: 200,
+        inspectable: 10,
+        closed: 12032
+      },
+      {
+        date: '2020-02-01',
+        open: 129,
+        inspectable: 80,
+        closed: 12132
+      },
+      {
+        date: '2020-02-02',
+        open: 80,
+        inspectable: 71,
+        closed: 12142
+      },
+      {
+        date: '2020-02-03',
+        open: 150,
+        inspectable: 21,
+        closed: 12202
+      },
+      {
+        date: '2020-02-04',
+        open: 240,
+        inspectable: 37,
+        closed: 12289
+      },
+      {
+        date: '2020-02-05',
+        open: 170,
+        inspectable: 142,
+        closed: 12364
+      }
+    ]
+
+    let labels = []
+    let open = []
+    let inspectable = []
+    let closed = []
+    let maxValue = 0;
+    exampleData.forEach(entry => {
+      labels.push(entry.date)
+      open.push(entry.open)
+      inspectable.push(entry.inspectable)
+      closed.push(entry.closed)
+
+      maxValue = Math.max(entry.open + entry.inspectable + entry.closed, maxValue)
+    })
+
+    const minValue = Math.min(...closed)
+    const diff = maxValue - minValue
+    const targetMin = Math.max(Math.round(minValue - diff / 5), 0)
+
+    const ctx = this.$refs.chart.getContext('2d')
     this.chart = new Chart(ctx, {
-      type: 'bar',
+      type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: labels,
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
+          label: 'closed',
+          type: 'line',
+          borderWidth: 1,
+          borderColor: '#28a745',
+          backgroundColor: '#bfe5c7',
+          data: closed,
+        }, {
+          label: 'to inspect',
+          type: 'line',
+          borderWidth: 1,
+          borderColor: '#ffc107',
+          backgroundColor: '#ffecb5',
+          data: inspectable,
+        },{
+          label: 'open',
+          type: 'line',
+          borderWidth: 1,
+          borderColor: '#343477',
+          backgroundColor: '#c2c2d6',
+          data: open,
+        } ]
       },
       options: {
+        legend: {
+          reverse: true
+        },
         scales: {
           yAxes: [{
+            stacked: true,
             ticks: {
-              beginAtZero: true
+              min: targetMin
             }
           }]
         }
       }
-    });
+    })
   }
 }
 </script>
