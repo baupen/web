f0f0386dff6a6d796a5c6aeb54c96bcf7f9c44f0
Florian Moser <git@famoser.ch>
2021-03-09 10:35:27 +0100

    Resolve #451

---
+++ tests/Controller/LegacyUrlControllerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the mangel.io project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Controller;
+
+use App\Entity\ConstructionManager;
+use App\Tests\DataFixtures\TestConstructionManagerFixtures;
+use App\Tests\DataFixtures\TestConstructionSiteFixtures;
+use App\Tests\Traits\AssertAuthenticationTrait;
+use App\Tests\Traits\AssertEmailTrait;
+use App\Tests\Traits\TestDataTrait;
+use Doctrine\Persistence\ManagerRegistry;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LegacyUrlControllerTest extends WebTestCase
+{
+    use FixturesTrait;
+    use AssertEmailTrait;
+    use AssertAuthenticationTrait;
+    use TestDataTrait;
+
+    public function testCanLoginCraftsman()
+    {
+        $client = $this->createClient();
+        $this->loadFixtures([TestConstructionManagerFixtures::class, TestConstructionSiteFixtures::class]);
+
+        $constructionSite = $this->getTestConstructionSite();
+        $craftsman = $constructionSite->getCraftsmen()[0];
+
+        $legacyUrl = "/external/share/c/".$craftsman->getAuthenticationToken()."?token=ed5aad1d-3698-49e8-baa3-71237127317";
+        $newUrl = "/resolve/".$craftsman->getAuthenticationToken();
+
+        $client->request('GET', $legacyUrl);
+        $this->assertResponseRedirects($newUrl);
+    }
+}
