daf9e9323a4ec9cc2242cf94b454f6ddfaf144b4
Florian Moser <git@famoser.ch>
2021-10-31 18:43:29 +0100

    Revert "Remove #362 workaround"
    This reverts commit 28c99f887e9486125094a22cd8b53b159838e347.

---
+++ src/Doctrine/Driver.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the baupen project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+ * This file is part of the baupen project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Doctrine;
+
+use Doctrine\DBAL\Driver\PDOMySql;
+
+class Driver extends PDOMySql\Driver
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDatabasePlatform()
+    {
+        return new Platform();
+    }
+}
---
+++ src/Doctrine/TrimSchemaEnvVarProcessor.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the baupen project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Doctrine;
+
+use Symfony\Component\DependencyInjection\EnvVarProcessorInterface;
+
+class TrimSchemaEnvVarProcessor implements EnvVarProcessorInterface
+{
+    public function getEnv(string $prefix, string $name, \Closure $getEnv)
+    {
+        $env = $getEnv($name);
+
+        return substr($env, strpos($env, '://') + 3);
+    }
+
+    public static function getProvidedTypes()
+    {
+        return [
+            'stripschema' => 'string',
+        ];
+    }
+}
