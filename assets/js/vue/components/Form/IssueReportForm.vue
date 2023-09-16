83d28751778078f649cf5cf46f0719c64ffcaadb
Florian Moser <git@famoser.ch>
2023-09-16 18:09:58 +0200

    refactor: Single menu for options

--- assets/js/vue/components/Form/IssueReportForm.vue
+++ assets/js/vue/components/Form/IssueReportForm.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="form-group">
+  <form-field :label="$t('_form.issue_report.content')" :required="false">
+    <custom-checkbox
+        class="mb-1"
+        for-id="report-table-by-craftsman"
+        :label="$t('_form.issue_report.table_by_craftsman')">
+      <input class="custom-control-input" type="checkbox" name="report-table-by-craftsman" value="selection"
+             id="report-table-by-craftsman"
+             :true-value="true" :false-value="false"
+             v-model="report.tableByCraftsman">
+    </custom-checkbox>
+
+    <custom-checkbox
+        class="mb-1"
+        for-id="report-table-by-map"
+        :label="$t('_form.issue_report.table_by_map')">
+      <input class="custom-control-input" type="checkbox" name="report-table-by-map" value="selection"
+             id="report-table-by-map"
+             :true-value="true" :false-value="false"
+             v-model="report.tableByMap">
+    </custom-checkbox>
     <custom-checkbox
         class="mb-1"
         for-id="report-with-renders"
@@ -14,27 +33,6 @@
       <input class="custom-control-input" type="checkbox" name="report-with-images" id="report-with-images"
              :true-value="true" :false-value="false"
              v-model="report.withImages">
-    </custom-checkbox>
-  </div>
-
-  <form-field :label="$t('_form.issue_report.summary_tables')" :required="false">
-    <custom-checkbox
-        class="mb-1"
-        for-id="report-table-by-craftsman"
-        :label="$t('_form.issue_report.by_craftsman')">
-      <input class="custom-control-input" type="checkbox" name="report-table-by-craftsman" value="selection"
-             id="report-table-by-craftsman"
-             :true-value="true" :false-value="false"
-             v-model="report.tableByCraftsman">
-    </custom-checkbox>
-
-    <custom-checkbox
-        for-id="report-table-by-map"
-        :label="$t('_form.issue_report.by_map')">
-      <input class="custom-control-input" type="checkbox" name="report-table-by-map" value="selection"
-             id="report-table-by-map"
-             :true-value="true" :false-value="false"
-             v-model="report.tableByMap">
     </custom-checkbox>
   </form-field>
 
