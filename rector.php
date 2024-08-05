f8e2ff4ec40068e058f7fb962d1df3d0d1d59b1c
Florian Moser <git@famoser.ch>
2024-08-05 09:34:00 +0200

    ci: Add rector

--- rector.php
+++ rector.php
@@ -13,12 +13,11 @@ use Rector\Config\RectorConfig;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 
 return RectorConfig::configure()
-// register single rule
     ->withRules([
         TypedPropertyFromStrictConstructorRector::class,
     ])
-// here we can define, what prepared sets of rules will be applied
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true
-    );
+    )
+    ->withPaths(["bin", "config", "migrations", "public", "src", "tests"]);
