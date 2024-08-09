21c6352cbac790190861e7ed0ee197692ae09343
Florian Moser <git@famoser.ch>
2024-08-09 13:34:55 +0200

    feat: Implement removal of recent entries

---
+++ assets/js/vue/components/Action/RemoveProtocolEntryButton.vue
@@ -0,0 +1,50 @@
+<template>
+  <button-with-modal-confirm
+      color="danger"
+      :title="$t('_action.remove_protocol_entry.title')" :can-confirm="canConfirm"
+      @confirm="confirm">
+    <template v-slot:button-content>
+      <font-awesome-icon :icon="['fal', 'trash']" />
+    </template>
+
+    <p class="alert alert-info">
+      {{ $t('_action.remove_protocol_entry.help') }}
+    </p>
+
+    <delete-form @update="canConfirm = $event" />
+
+  </button-with-modal-confirm>
+</template>
+
+<script>
+
+import { api } from '../../services/api'
+import ButtonWithModalConfirm from '../Library/Behaviour/ButtonWithModalConfirm'
+import DeleteForm from '../Form/DeleteForm'
+import { displaySuccess } from '../../services/notifiers'
+
+export default {
+  emits: ['removed'],
+  components: {
+    DeleteForm,
+    ButtonWithModalConfirm
+  },
+  data () {
+    return {
+      canConfirm: false,
+    }
+  },
+  props: {
+    protocolEntry: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    confirm: function () {
+      api.delete(this.protocolEntry, this.$t('_action.remove_protocol_entry.removed'))
+          .then(_ => this.$emit('removed'))
+    },
+  }
+}
+</script>
