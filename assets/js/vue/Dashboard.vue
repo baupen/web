3699baa189f1994c07b99643525a402dfcec4ceb
Florian Moser <git@famoser.ch>
2024-11-09 16:08:26 +0100

    Revert "feat: Hide protocol and TODOs for now"
    This reverts commit c6292a668eaa1f157368ef2ea809992c403cef2d.

--- assets/js/vue/Dashboard.vue
+++ assets/js/vue/Dashboard.vue
@@ -5,6 +5,10 @@
         <div class="col-md-12 col-lg-4">
           <h3>{{ $t('construction_site._name') }}</h3>
           <dashboard-construction-site class="shadow mb-4" :construction-site="constructionSite"/>
+          <dashboard-protocol
+              class="shadow"
+              :construction-managers="constructionManagers" :construction-site="constructionSite"
+              :construction-manager-iri="constructionManagerIri"/>
         </div>
         <div class="col-md-6 col-lg-4">
           <h3>{{ $t('issue._plural') }}</h3>
@@ -15,6 +19,9 @@
           <h3>{{ $t('dashboard.activity') }}</h3>
           <dashboard-feed class="shadow mb-4" :construction-site="constructionSite"
                           :construction-managers="constructionManagers"/>
+          <dashboard-tasks class="shadow"
+                           :construction-site="constructionSite" :construction-managers="constructionManagers"
+                           :construction-manager-iri="constructionManagerIri" />
         </div>
       </div>
     </loading-indicator>
