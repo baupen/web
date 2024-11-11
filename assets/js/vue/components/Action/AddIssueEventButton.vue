6df6ebf301b7f08cea7b4811f88484874e5d7903
Florian Moser <git@famoser.ch>
2024-11-11 08:39:18 +0100

    feat: Abstract field

--- assets/js/vue/components/Action/AddIssueEventButton.vue
+++ assets/js/vue/components/Action/AddIssueEventButton.vue
@@ -5,34 +5,7 @@
       @confirm="confirm"
   >
     <slot name="before-form"/>
-    <div class="mb-3">
-      <custom-radio-field
-          for-id="issue-event-type-text"
-          :label="$t('_action.add_issue_event.type_text')">
-        <input
-            id="issue-event-type-text" class="form-check-input" type="radio"
-            name="issue-event-type" value="TEXT"
-            v-model="entryType">
-      </custom-radio-field>
-
-      <custom-radio-field
-          for-id="issue-event-type-image"
-          :label="$t('_action.add_issue_event.type_image')">
-        <input
-            id="issue-event-type-image" class="form-check-input" type="radio"
-            name="issue-event-type" value="IMAGE"
-            v-model="entryType">
-      </custom-radio-field>
-
-      <custom-radio-field
-          for-id="issue-event-type-file"
-          :label="$t('_action.add_issue_event.type_file')">
-        <input
-            id="issue-event-type-file" class="form-check-input" type="radio"
-            name="issue-event-type" value="FILE"
-            v-model="entryType">
-      </custom-radio-field>
-    </div>
+    <issue-event-entry-type-checkbox class="mb-3" v-model="entryType" />
 
     <template v-if="entryType === 'TEXT'">
       <issue-event-text-form @update="post = $event" :template="staleTemplate" :text-mode="true"/>
@@ -59,9 +32,11 @@ import MapForm from "../Form/MapForm.vue";
 import FileForm from "../Form/FileForm.vue";
 import ImageForm from "../Form/ImageForm.vue";
 import CustomRadioField from "../Library/FormLayout/CustomRadioField.vue";
+import IssueEventEntryTypeCheckbox from "../Form/Field/IssueEventEntryTypeCheckbox.vue";
 
 export default {
   components: {
+    IssueEventEntryTypeCheckbox,
     CustomRadioField,
     ImageForm,
     FileForm, MapForm,
