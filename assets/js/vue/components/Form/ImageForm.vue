18729f27c146f6ee46a9b34552a113b8ab99aac0
Florian Moser <git@famoser.ch>
2025-05-28 14:55:59 +0200

    feat: Add preview to maps and images

--- assets/js/vue/components/Form/ImageForm.vue
+++ assets/js/vue/components/Form/ImageForm.vue
@@ -1,16 +1,24 @@
 <template>
   <form-field for-id="image" :label="$t('_form.image.label')" :required="false">
-    <dropzone
-        v-if="!image"
-        id="image" :help="dropHelp"
-        :valid-file-types="validFileTypes"
-        @input="image = $event[0]" />
-    <input v-if="image" id="image" class="form-control" type="text" readonly="readonly"
-           :class="{'is-valid': imageIsValid, 'is-invalid': !imageIsValid }"
-           :value="image.name">
-    <a class="btn-link clickable" v-if="image" @click="image = null">
-      {{ $t('_form.reset') }}
-    </a>
+    <div class="row g-2 dropzone-height">
+      <div class="col-md-4" v-if="currentUrl">
+        <img class="img-fluid" :src="currentUrl" alt="preview">
+      </div>
+      <div :class="{'col-md-8': currentUrl, 'col-md-12': !currentUrl}">
+        <dropzone
+            class="h-100"
+            v-if="!image"
+            id="image" :help="dropHelp"
+            :valid-file-types="validFileTypes"
+            @input="image = $event[0]"/>
+        <input v-if="image" id="image" class="form-control" type="text" readonly="readonly"
+               :class="{'is-valid': imageIsValid, 'is-invalid': !imageIsValid }"
+               :value="image.name">
+        <a class="btn-link clickable" v-if="image" @click="image = null">
+          {{ $t('_form.reset') }}
+        </a>
+      </div>
+    </div>
   </form-field>
 </template>
 
@@ -18,7 +26,7 @@
 
 import FormField from '../Library/FormLayout/FormField'
 import Dropzone from '../Library/FormInput/Dropzone'
-import { validImageTypes } from '../../services/api'
+import {validImageTypes} from '../../services/api'
 import {isSafari} from "../../services/utils";
 
 export default {
@@ -31,6 +39,12 @@ export default {
     return {
       image: null
     }
+  },
+  props: {
+    currentUrl: {
+      type: String,
+      required: false,
+    },
   },
   watch: {
     image: function () {
