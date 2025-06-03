a92f2030bffb1d7d10cc4cb35052c314b99d6a2e
Florian Moser <git@famoser.ch>
2025-06-03 10:49:24 +0200

    maint: Cleanup environement

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 var Encore = require('@symfony/webpack-encore')
-const path = require('path');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
