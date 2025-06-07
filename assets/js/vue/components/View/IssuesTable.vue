4492aabdc740a91ec5f9a0f39848f8fcfb58595a
Florian Moser <git@famoser.ch>
2025-06-07 15:55:09 +0200

    fix: Do not store filters over different users

--- assets/js/vue/components/View/IssuesTable.vue
+++ assets/js/vue/components/View/IssuesTable.vue
@@ -332,7 +332,7 @@ export default {
     },
     persistFilterKey: function () {
       if (this.view === "foyer" || this.view === "register") {
-        return 'issues-table-' + this.view + '-' + this.constructionSite['@id']
+        return 'issues-table-' + this.view + '-' + this.constructionSite['@id'] + '-' + this.constructionManagerIri
       }
       return null
     },
