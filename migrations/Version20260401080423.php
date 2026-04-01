7ad74e61e7c40ff9539d401501073a1b9580b32f
Florian Moser <git@famoser.ch>
2026-04-01 10:08:52 +0200

    feat: Add craftsman fields

---
+++ migrations/Version20260401080423.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260401080423 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE craftsman ADD contact_job_title VARCHAR(255) NOT NULL, ADD telephone LONGTEXT NOT NULL, ADD address LONGTEXT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE craftsman DROP contact_job_title, DROP telephone, DROP address');
+    }
+}
