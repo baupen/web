828043f9d3068fc51ed94831f5c00d2add2545e7
Florian Moser <git@famoser.ch>
2026-04-01 10:28:32 +0200

    ref: Make new fields optional

--- migrations/Version20260401082737.php
+++ migrations/Version20260401082737.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260401080423 extends AbstractMigration
+final class Version20260401082737 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20260401080423 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE craftsman ADD contact_job_title VARCHAR(255) NOT NULL, ADD telephone LONGTEXT NOT NULL, ADD address LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE craftsman ADD contact_job_title VARCHAR(255) DEFAULT NULL, ADD telephone LONGTEXT DEFAULT NULL, ADD address LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
