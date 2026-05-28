dfad876bfe979ac9690b362519884f7ac6684697
Florian Moser <git@famoser.ch>
2026-05-28 07:34:51 +0200

    ref: Remove useless header

--- src/Api/Filters/PatchedOrderFilter.php
+++ src/Api/Filters/PatchedOrderFilter.php
@@ -11,15 +11,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the baupen project.
- *
- * (c) Florian Moser <git@famoser.ch>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace App\Api\Filters;
 
 use ApiPlatform\Core\Bridge\Doctrine\Common\Filter\OrderFilterInterface;
--- src/Helper/IssueHelper.php
+++ src/Helper/IssueHelper.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the baupen project.
- *
- * (c) Florian Moser <git@famoser.ch>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace App\Helper;
 
 use App\Entity\Craftsman;
