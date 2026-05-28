1bd1a443cc4647d57ddd08e6775fe46a505a94a8
Florian Moser <git@famoser.ch>
2026-05-28 07:34:51 +0200

    ref: Remove useless header

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,14 +9,7 @@
  * file that was distributed with this source code.
  */
 
-$fileHeaderComment = <<<COMMENT
-This file is part of the baupen project.
-
-(c) Florian Moser <git@famoser.ch>
-
-For the full copyright and license information, please view the LICENSE
-file that was distributed with this source code.
-COMMENT;
+$fileHeaderComment = '';
 
 $finder = PhpCsFixer\Finder::create()
     ->in('src')
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
- * This file is part of the baupen project.
- *
- * (c) Florian Moser <git@famoser.ch>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\ErrorHandler;
