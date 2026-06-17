28beab3e57fd10606e6e09aa5238cba9e05bdebd
Florian Moser <git@famoser.ch>
2026-06-17 09:23:39 +0200

    fix: Recover loading behavior for edit

--- assets/js/vue/components/EditConstructionManagers.vue
+++ assets/js/vue/components/EditConstructionManagers.vue
@@ -1,22 +1,26 @@
 <template>
-  <h2 class="mt-5">{{ $t('construction_manager._plural') }}</h2>
-  <p>{{ $t('edit.construction_managers_help') }}</p>
-  <div class="btn-group" v-if="constructionManager?.canAssociateSelf">
-    <associate-construction-manager-button :construction-site="constructionSite" @added="constructionManagers.push($event)" />
-  </div>
-  <p class="alert alert-info" v-else>
-    {{ $t('edit.construction_managers_disabled') }}
-  </p>
-  <construction-manager-association-table class="mt-2" :construction-site="constructionSite" :construction-managers="constructionManagers" :self-construction-manager="constructionManager" @removed="remove" />
+  <loading-indicator :spin="!constructionManagers">
+    <h2 class="mt-5">{{ $t('construction_manager._plural') }}</h2>
+    <p>{{ $t('edit.construction_managers_help') }}</p>
+    <div class="btn-group" v-if="constructionManager?.canAssociateSelf">
+      <associate-construction-manager-button :construction-site="constructionSite" @added="constructionManagers.push($event)" />
+    </div>
+    <p class="alert alert-info" v-else>
+      {{ $t('edit.construction_managers_disabled') }}
+    </p>
+    <construction-manager-association-table class="mt-2" :construction-site="constructionSite" :construction-managers="constructionManagers" :self-construction-manager="constructionManager" @removed="remove" />
+  </loading-indicator>
 </template>
 
 <script>
 import { api } from '../domain/api'
 import AssociateConstructionManagerButton from './Action/AssociateConstructionManagerButton'
 import ConstructionManagerAssociationTable from './View/ConstructionManagerAssociationTable'
+import LoadingIndicator from './Library/View/LoadingIndicator.vue'
 
 export default {
   components: {
+    LoadingIndicator,
     ConstructionManagerAssociationTable,
     AssociateConstructionManagerButton,
   },
@@ -37,7 +41,7 @@ export default {
   },
   computed: {
     constructionManager: function () {
-      return this.constructionManagers?.find(cm => cm.iri === this.constructionManagerIri)
+      return this.constructionManagers?.find(cm => cm['@id'] === this.constructionManagerIri)
     }
   },
   methods: {
