04c856960ae4d4ec9dbc314f3a25d5b85af557d4
Florian Moser <git@famoser.ch>
2026-06-27 07:53:44 +0200

    fix: Fix click behaviour when clicking on checkbox

--- assets/js/vue/components/Form/IssueFilter/CraftsmenFilter.vue
+++ assets/js/vue/components/Form/IssueFilter/CraftsmenFilter.vue
@@ -2,7 +2,7 @@
   <div>
     <custom-checkbox-field
         id="filter-all-craftsmen"
-        @click.prevent="toggleAllEntitiesSelected"
+        @click="toggleAllEntitiesSelected"
         :label="$t('_form.issue_filter.all_craftsmen')">
       <input class="form-check-input" type="checkbox"
              :indeterminate.prop="selectedEntities.length > 0 && !allEntitiesSelected"
